2ea858b97aa5d8a1e0238bdd1b4756a1cf62f113
Daniel Dehesa <daniel.dehesamoreno@iesmiguelherrero.com>
2021-09-17 17:33:19 +0200

    Final de Clase

--- Hoja01_Ficheros_03_Ejer1/src/main/java/es/danieldehesa/hoja01_ficheros_03_ejer1/Ej1.java
+++ Hoja01_Ficheros_03_Ejer1/src/main/java/es/danieldehesa/hoja01_ficheros_03_ejer1/Ej1.java
@@ -1,5 +1,12 @@
 package es.danieldehesa.hoja01_ficheros_03_ejer1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author usuario
@@ -7,6 +14,20 @@ package es.danieldehesa.hoja01_ficheros_03_ejer1;
 public class Ej1 {
 
     public static void main(String[] args){
+        Scanner teclado= new Scanner(System.in);
+        System.out.println("¿Que fichero quieres usar?");
+        String fichero=teclado.nextLine();
+        System.out.println("¿Que texto quieres añadir al fichero?");
+        String texto=teclado.nextLine();
+        File f = new File(fichero);
+        try{
+            PrintWriter escritor = new PrintWriter(new FileWriter(fichero));
+            
+        }catch (FileNotFoundException fn){
+            System.out.println("No se encuentra el fichero");
+        }catch (IOException io){
+            System.out.println("Error de E/S");
+        }
         
     }
 }
